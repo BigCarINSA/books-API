8a6b5fb445d5454f06f3a048cde3ed69fc7aac3f
Son <sonngotaiinsa@gmail.com>
2023-07-18 22:08:15 +0700

    add buy book API

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const booksController = require('../controllers/booksController');
 
-router.get('/', booksController.getAllBooks);
 router.post('/store', booksController.addBook);
 router.get('/:id', booksController.getBookById);
 router.put('/:id/update', booksController.updateById);
 router.delete('/:id', booksController.deleteById);
+router.post('/:id/buy', booksController.buyBookById);
+router.get('/', booksController.getAllBooks);
 
 module.exports = router;
